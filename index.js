8df194459478df39ddc9220797d4887e4c436af3
Allou Mohamed <118113221+Varnosbit@users.noreply.github.com>
2024-07-20 20:57:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-
-const app = express();
 const port = 3000;
 const app = express();
 app.get('/', (req, res) => res.sendFile(__dirname + '/allou.html'));
