77ad4f7e1670585eb329662ef1e247976391bbf6
Allou Mohamed <118113221+Varnosbit@users.noreply.github.com>
2024-07-20 08:22:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
-const app = express();
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
